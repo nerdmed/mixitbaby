0f1c9c17bd4cd96524d322fae275b766e1ba8d67
Ahmed Hassan <a.hassan@buismed.de>
2015-01-31 17:03:34 +0100

    inital commit

---
+++ client/views/main/main.js
@@ -0,0 +1,32 @@
+/*****************************************************************************/
+/* Main: Event Handlers and Helpersss .js*/
+/*****************************************************************************/
+Template.Main.events({
+  /*
+   * Example:
+   *  'click .selector': function (e, tmpl) {
+   *
+   *  }
+   */
+});
+
+Template.Main.helpers({
+  /*
+   * Example:
+   *  items: function () {
+   *    return Items.find();
+   *  }
+   */
+});
+
+/*****************************************************************************/
+/* Main: Lifecycle Hooks */
+/*****************************************************************************/
+Template.Main.created = function () {
+};
+
+Template.Main.rendered = function () {
+};
+
+Template.Main.destroyed = function () {
+};
---
+++ client/views/player_deck/player_deck.js
@@ -0,0 +1,58 @@
+/*****************************************************************************/
+/* PlayerDeck: Event Handlers and Helpersss .js*/
+/*****************************************************************************/
+Template.PlayerDeck.events({
+    'click .play-pause ': function(e, tpl) {
+        tpl.player.playing.get() ? tpl.player.pause() : tpl.player.play();
+    }
+});
+
+Template.PlayerDeck.helpers({
+    playing: function() {
+        var instance = Template.instance();
+        var playing = instance.player.playing.get();
+        if (playing === true) return "playing";
+        else if (playing === false) return "paused";
+    },
+    playingClass: function() {
+        var instance = Template.instance();
+        var playing = instance.player.playing.get();
+        if (playing === false) return "glyphicon-play";
+        else if (playing === true) return "glyphicon-pause";
+    }
+});
+
+/*****************************************************************************/
+/* PlayerDeck: Lifecycle Hooks */
+/*****************************************************************************/
+Template.PlayerDeck.created = function() {
+    this.player = new PlayerDeck();
+};
+
+
+
+/*****************************************************************************/
+/* PlayerDeck: Object */
+/*****************************************************************************/
+function PlayerDeck(obj) {
+    var obj = obj || {};
+    this.songId = new ReactiveVar();
+    this.audio = obj.audio || new Audio("/music.mp3");
+    this.loading = new ReactiveVar();
+    this.source = new ReactiveVar();
+    this.playing = new ReactiveVar(false);
+}
+
+_.extend(PlayerDeck.prototype, {
+    play: function() {
+        this.audio.play();
+        this.playing.set(true);
+    },
+    pause: function() {
+        this.audio.pause();
+        this.playing.set(false);
+    },
+    addQPoint: function() {
+
+    }
+})
